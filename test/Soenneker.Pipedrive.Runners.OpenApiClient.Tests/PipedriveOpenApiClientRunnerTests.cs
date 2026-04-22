4ba578836508ba2e5c44ef82f21411d008459486
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:03:30 -0500

    TUnit conversion

--- test/Soenneker.Pipedrive.Runners.OpenApiClient.Tests/PipedriveOpenApiClientRunnerTests.cs
+++ test/Soenneker.Pipedrive.Runners.OpenApiClient.Tests/PipedriveOpenApiClientRunnerTests.cs
@@ -1,16 +1,15 @@
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Pipedrive.Runners.OpenApiClient.Tests;
 
-[Collection("Collection")]
-public sealed class PipedriveOpenApiClientRunnerTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class PipedriveOpenApiClientRunnerTests : HostedUnitTest
 {
-    public PipedriveOpenApiClientRunnerTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public PipedriveOpenApiClientRunnerTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
